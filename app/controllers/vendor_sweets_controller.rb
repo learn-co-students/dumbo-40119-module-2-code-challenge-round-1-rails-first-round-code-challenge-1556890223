1080a05f15c481697e42db2fa9c054414ee16b2e
melonybox <grandmadcow@gmail.com>
2019-05-03 11:10:29 -0400

    Did my best

---
+++ app/controllers/vendor_sweets_controller.rb
@@ -0,0 +1,28 @@
+class VendorSweetsController < ApplicationController
+  def new
+    @vendorsweet = VendorSweet.new
+  end
+
+  def create
+    #@vendor = Vendor.find(params[:vendor_id])
+    @vendorsweet = VendorSweet.new(vendorsweet_params)
+    if VendorSweet.create
+      redirect_to vendor_path(@vendorsweet.vendor_id)
+    else
+      render :new
+    end
+  end
+
+  #Create works by doing a model.new
+  #then a if else statement
+  #the if the transaction for creation is valid it will redirect to the vendors page
+  #if not valid it will render :new again with the error messages displayed
+
+  #is validating in the cmd but the syntax above does not work.
+
+  private
+
+  def vendorsweet_params
+    params.require(:vendor_sweet).permit(:sweet_id,:vendor_id,:price)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :vendors
+  resources :vendor_sweets
+  resources :sweets
+
+  #Refactor later to contain only needed paths
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/sweets', to: "sweets#index", as: "sweets"
-  get '/vendors', to: "vendors#index", as: "vendors"
+
 
 end

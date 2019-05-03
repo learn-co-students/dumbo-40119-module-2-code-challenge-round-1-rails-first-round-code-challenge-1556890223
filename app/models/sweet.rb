21acb691b91e12a5f180ff1556b85e11cfc74284
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-05-03 09:48:00 -0400

    migrations done

--- app/models/sweet.rb
+++ app/models/sweet.rb
@@ -1,3 +1,4 @@
 class Sweet < ApplicationRecord
-
+  has_many :vendor_sweets, dependent: :delete_all
+  has_many :vendors, through: :vendor_sweets
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,4 @@
 class Vendor < ApplicationRecord
-
+  has_many :vendor_sweets, dependent: :delete_all
+  has_many :sweets, through: :vendor_sweets
 end

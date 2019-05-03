30f84fcc6ad0f46b0f572c17d8b87e8be87c2f76
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-05-03 11:09:58 -0400

    added mailto

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,10 @@
 class Vendor < ApplicationRecord
   has_many :vendor_sweets, dependent: :delete_all
   has_many :sweets, through: :vendor_sweets
+
+  def email
+    @alias = self.name.to_s.split().join(".").downcase
+    return "#{@alias}@candy_kingdom.com"
+  end
+
 end

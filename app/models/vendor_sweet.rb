54aa647fffed8d03f65c478bd7cf5abf9d5d62cc
Mallory Woods <malvwoods@gmail.com>
2019-05-03 11:05:35 -0400

    added final stretch features, adjusted formatting, corrected spelling errors.

--- app/models/vendor_sweet.rb
+++ app/models/vendor_sweet.rb
@@ -5,5 +5,5 @@ class VendorSweet < ApplicationRecord
   validates_presence_of :price
   validates :price, numericality: { only_integer: true }
   validates :price, numericality: { greater_than: 0 }
-
+  validates_uniqueness_of :vendor_id, scope: :sweet_id, message: "already carries this variety of sweet. Please choose another."
 end

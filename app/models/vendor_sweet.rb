e1874e74d4295c5ff70e2de4cab373d896a9a222
github username <github email address>
2019-05-03 11:10:59 -0400

    final one?

--- app/models/vendor_sweet.rb
+++ app/models/vendor_sweet.rb
@@ -6,6 +6,7 @@ class VendorSweet < ApplicationRecord
     validates_numericality_of :price, {
         greater_than: 0
     } 
-    
-    validates_uniqueness_of :sweet_id, :vendor_id
+    #this checks for validation of respective id's // incomplete method
+    #validates_uniqueness_of :sweet_id, :message => "is sold by this vendor already."
+    #validates_uniqueness_of :vendor_id, :message => "sells this sweet already."
 end
